14737f303a59b0d5df767b919b73de47c385cf94
Aleksey Kuznetsov <neiro.mail@gmail.com>
2013-04-14 13:09:09 +0400

    Create new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kron::Application.config.secret_token = 'fc7a2e79887cd9fc077e3bf09b8e9f694c56261e86b686a2fa06ab8e0cf58451cd8d719c3f2106de87bea5265503615caae12132710d16e90a84406bebc5aa80'
